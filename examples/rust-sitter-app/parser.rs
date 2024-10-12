7e3f569f30f2164fda4c421fd9447c40c9922928
Jay Pratt <jp10010101010000@gmail.com>
2024-10-13 10:38:59 +1100

    TMP

---
+++ examples/rust-sitter-app/parser.rs
@@ -0,0 +1,41 @@
+use std::collections::HashMap;
+use std::str;
+
+#[derive(Debug, PartialEq)]
+pub enum JsonValue {
+    Null,
+    Str(String),
+    Boolean(bool),
+    Num(f64),
+    Array(Vec<JsonValue>),
+    Object(HashMap<String, JsonValue>),
+}
+
+#[rust_sitter::grammar("parser")]
+pub mod grammar {
+    #[rust_sitter::language]
+    #[derive(PartialEq, Eq, Debug)]
+    pub enum Expression {
+        Number(#[rust_sitter::leaf(pattern = r"\d+", transform = |v| v.parse().unwrap())] i32),
+        #[rust_sitter::prec_left(1)]
+        Sub(
+            Box<Expression>,
+            #[rust_sitter::leaf(text = "-")] (),
+            Box<Expression>,
+        ),
+        #[rust_sitter::prec_left(2)]
+        Mul(
+            Box<Expression>,
+            #[rust_sitter::leaf(text = "*")] (),
+            Box<Expression>,
+        ),
+    }
+
+    #[rust_sitter::extra]
+    struct Whitespace {
+        #[rust_sitter::leaf(pattern = r"\s")]
+        _whitespace: (),
+    }
+}
+
+
